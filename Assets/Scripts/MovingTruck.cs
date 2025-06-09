888d328047349e9f24efcba961e8249870c3f174
10267815E <s10267815@connect.np.edu.sg>
2025-06-09 14:29:31 +0800

    implemented moving object code, where object will move from point A to point B and vice versa

--- Assets/Scripts/MovingTruck.cs
+++ Assets/Scripts/MovingTruck.cs
@@ -2,15 +2,33 @@ using UnityEngine;
 
 public class MovingTruck : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public Transform pointA;
+    public Transform pointB;
+    public float speed = 5f;
+
+    private Vector3 target;
+
     void Start()
     {
-        
+        target = pointB.position;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        // Move truck toward the target point
+        transform.position = Vector3.MoveTowards(transform.position, target, speed * Time.deltaTime);
+
+        // Switch target if reached
+        if (Vector3.Distance(transform.position, target) < 0.1f)
+        {
+            if (target == pointB.position)
+            {
+                target = pointA.position;
+            }
+            else
+            {
+                target = pointB.position;
+            }
+        }
     }
 }
